016b857d8f8f0f000fbb0a8c53fa382f77c24d19
murrani <murrani@mail.upb.de>
2013-12-20 01:36:32 -0800

    some constraints functions and UI were defined in EScript

--- Ext/Physics/E_PhysicWorld.cpp
+++ Ext/Physics/E_PhysicWorld.cpp
@@ -57,11 +57,20 @@ void E_PhysicWorld::init(EScript::Namespace & lib) {
 	//! [ESMF] thisEObj PhysicWorld.addNodeToPhyiscWorld(Node)
 	ES_MFUN(typeObject, PhysicWorld, "addNodeToPhyiscWorld", 1, 1,	(thisObj->addNodeToPhyiscWorld(parameter[0].to<MinSG::Node*>(rt)),thisEObj))
 
+    //! [ESMF] thisEObj PhysicWorld.applyHingeConstraint(Node, Node, Geometry::Vec3, Geometry::Vec3)
+	ES_MFUN(typeObject, PhysicWorld, "applyHingeConstraint", 4, 4, (thisObj->applyHingeConstraint(parameter[0].to<MinSG::Node*>(rt), parameter[1].to<MinSG::Node*>(rt), parameter[2].to<const Geometry::Vec3&>(rt), parameter[3].to<const Geometry::Vec3&>(rt)),thisEObj))
+
+	//! [ESMF] thisEObj PhysicWorld.applyP2PConstraint(Node, Node, Geometry::Vec3)
+	ES_MFUN(typeObject, PhysicWorld, "applyP2PConstraint", 3, 3, (thisObj->applyP2PConstraint(parameter[0].to<MinSG::Node*>(rt), parameter[1].to<MinSG::Node*>(rt), parameter[2].to<const Geometry::Vec3&>(rt)),thisEObj))
+
 	//! [ESMF] thisEObj PhysicWorld.cleanupWorld()
 	ES_MFUN(typeObject, PhysicWorld, "cleanupWorld", 0, 0,			(thisObj->cleanupWorld(),thisEObj))
 
 	//! [ESMF] thisEObj PhysicWorld.createGroundPlane(Geometry::Plane)
 	ES_MFUN(typeObject, PhysicWorld, "createGroundPlane", 1, 1,		(thisObj->createGroundPlane(parameter[0].to<const Geometry::Plane&>(rt)),thisEObj))
+
+	//! [ESMF] String PhysicWorld.getConstraintPivot(Node)
+	ES_MFUN(typeObject, PhysicWorld, "getConstraintPivot", 1, 1,	(thisObj->getConstraintPivot(parameter[0].to<MinSG::Node*>(rt))))
 
 	//! [ESMF] Number PhysicWorld.getFriction(Node)
 	ES_MFUN(typeObject, PhysicWorld, "getFriction", 1, 1,			(thisObj->getFriction(parameter[0].to<MinSG::Node*>(rt))))
@@ -84,7 +93,13 @@ void E_PhysicWorld::init(EScript::Namespace & lib) {
 	 //! [ESMF] thisEObj PhysicWorld.initNodeObserver(Node)
 	ES_MFUN(typeObject, PhysicWorld, "initNodeObserver", 1, 1,		(thisObj->initNodeObserver(parameter[0].to<MinSG::Node*>(rt)),thisEObj))
 
-	 //! [ESMF] thisEObj PhysicWorld.removeNode(Node)
+	//! [ESMF] thisEObj PhysicWorld.removeConstraints(Node)
+	ES_MFUN(typeObject, PhysicWorld, "removeConstraints", 1, 1,	(thisObj->removeConstraints(parameter[0].to<MinSG::Node*>(rt)),thisEObj))
+
+	//! [ESMF] thisEObj PhysicWorld.removeConstraintBetweenNodes(Node, Node)
+	ES_MFUN(typeObject, PhysicWorld, "removeConstraintBetweenNodes", 2, 2,	(thisObj->removeConstraintBetweenNodes(parameter[0].to<MinSG::Node*>(rt), parameter[1].to<MinSG::Node*>(rt)),thisEObj))
+
+	//! [ESMF] thisEObj PhysicWorld.removeNode(Node)
 	ES_MFUN(typeObject, PhysicWorld, "removeNode", 1, 1,			(thisObj->removeNode(parameter[0].to<MinSG::Node*>(rt)),thisEObj))
 
 	 //! [ESMF] thisEObj PhysicWorld.renderPhysicWorld(RenderingContext)
@@ -95,6 +110,9 @@ void E_PhysicWorld::init(EScript::Namespace & lib) {
 
    //! [ESMF] thisEObj PhysicWorld.stepSimulation(float)
 	ES_MFUN(typeObject, PhysicWorld, "stepSimulation", 1, 1,		(thisObj->stepSimulation(parameter[0].toFloat()),thisEObj))
+
+	//! [ESMF] thisEObj PhysicWorld.updateConstraintPivot(Node, string)
+	ES_MFUN(typeObject, PhysicWorld, "updateConstraintPivot", 2, 2,(thisObj->updateConstraintPivot(parameter[0].to<MinSG::Node*>(rt), parameter[1].toString()),thisEObj))
 
 	//! [ESMF] thisEObj PhysicWorld.updateFriction(Node, float)
 	ES_MFUN(typeObject, PhysicWorld, "updateFriction", 2, 2,		(thisObj->updateFriction(parameter[0].to<MinSG::Node*>(rt), parameter[1].toFloat() ),thisEObj))
@@ -111,6 +129,7 @@ void E_PhysicWorld::init(EScript::Namespace & lib) {
 	//! [ESMF] thisEObj PhysicWorld.updateShape(Node, Map)
 	ES_MFUN(typeObject, PhysicWorld, "updateShape", 2, 2,			(thisObj->updateShape(parameter[0].to<MinSG::Node*>(rt),
 								  dynamic_cast< Util::GenericAttributeMap*>( E_Util::E_Utils::convertEScriptObjectToGenericAttribute( parameter[1].to<EScript::Map*>(rt) ))),thisEObj))
+
 
 
 }
