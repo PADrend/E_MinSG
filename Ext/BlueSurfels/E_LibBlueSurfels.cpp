ac366fc1482fa21309645ab1f3609850994ffb9d
myeti <myeti@mail.upb.de>
2018-12-02 15:25:47 +0100

    BlueSurfels: updated bindings

--- Ext/BlueSurfels/E_LibBlueSurfels.cpp
+++ Ext/BlueSurfels/E_LibBlueSurfels.cpp
@@ -65,13 +65,17 @@ void BlueSurfels::init(EScript::Namespace & lib) {
 	ES_FUN(nsBlueSurfels,"getMedianOfNthClosestNeighbours",3,3,		
 			MinSG::BlueSurfels::getMedianOfNthClosestNeighbours(*parameter[0].to<Rendering::Mesh*>(rt),parameter[1].to<size_t>(rt),parameter[2].to<size_t>(rt)))
 				
-	//! [ESMF] Number MinSG.BlueSurfels.getPrefixForRadius(float radius, float medianDist, uint32_t medianCount, uint32_t maxCount)
-	ES_FUN(nsBlueSurfels,"getPrefixForRadius",4,4,		
-			MinSG::BlueSurfels::getPrefixForRadius(parameter[0].toFloat(),parameter[1].toFloat(),parameter[2].toUInt(),parameter[3].toUInt()))
+	//! [ESMF] Number MinSG.BlueSurfels.getPrefixForRadius(float radius, float packing)
+	ES_FUN(nsBlueSurfels,"getPrefixForRadius",2,2,		
+			MinSG::BlueSurfels::getPrefixForRadius(parameter[0].toFloat(),parameter[1].toFloat()))
 			
-	//! [ESMF] Number MinSG.BlueSurfels.getRadiusForPrefix(uint32_t prefixLength, float medianDist, uint32_t medianCount)
-	ES_FUN(nsBlueSurfels,"getRadiusForPrefix",3,3,		
-			MinSG::BlueSurfels::getRadiusForPrefix(parameter[0].toUInt(),parameter[1].toFloat(),parameter[2].toUInt()))
+	//! [ESMF] Number MinSG.BlueSurfels.getRadiusForPrefix(uint32_t prefix, float packing)
+	ES_FUN(nsBlueSurfels,"getRadiusForPrefix",2,2,		
+			MinSG::BlueSurfels::getRadiusForPrefix(parameter[0].toUInt(),parameter[1].toFloat()))
+			
+	//! [ESMF] Number MinSG.BlueSurfels.computeSurfelPacking(Rendering.Mesh)
+	ES_FUN(nsBlueSurfels,"computeSurfelPacking",1,1,
+			MinSG::BlueSurfels::computeSurfelPacking(parameter[0].to<Rendering::Mesh*>(rt)))
 			
 	//! [ESMF] Number MinSG.BlueSurfels.getMeterPerPixel(MinSG::FrameContext & context, MinSG::Node * node)
 	ES_FUN(nsBlueSurfels,"getMeterPerPixel",2,2,		
