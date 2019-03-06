9fb111b311140958de6c2b37ce8d8aabfbc946ce
Sascha Brandt <myeti@mail.uni-paderborn.de>
2019-03-06 10:21:36 +0100

    BlueSurfels: bindings for dyn. size flag

--- Ext/BlueSurfels/Strategies/E_ShaderStrategy.cpp
+++ Ext/BlueSurfels/Strategies/E_ShaderStrategy.cpp
@@ -41,6 +41,7 @@ void E_ShaderStrategy::init(EScript::Namespace & lib) {
   ES_MGETSET(ShaderStrategy, std::string, ShaderFS)
   ES_MGETSET(ShaderStrategy, std::string, ShaderGS)
   ES_MGETSET(ShaderStrategy, bool, SurfelCulling)
+  ES_MGETSET(ShaderStrategy, bool, SurfelDynSize)
 	
 	ES_MFUN(typeObject, ShaderStrategy, "addSearchPath", 1, 1, (thisObj->getFileLocator().addSearchPath(parameter[0].toString()), thisEObj))
 }
